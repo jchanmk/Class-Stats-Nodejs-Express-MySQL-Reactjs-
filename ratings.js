2b6e5467ead1acb32efe400625d159d9b08faef6
Julian <julianmkchan@gmail.com>
2019-11-07 22:43:24 -0800

    figured out how to send data to server based on user input for star ratings, this is after I reconfigured the star components into react components instead of functions

--- ratings.js
+++ ratings.js
@@ -14,9 +14,9 @@ var StarList = function (_React$Component) {
     function StarList(props) {
         _classCallCheck(this, StarList);
 
-        console.log(props);
-
         var _this = _possibleConstructorReturn(this, (StarList.__proto__ || Object.getPrototypeOf(StarList)).call(this, props));
+        // console.log(props)
+
 
         _this.mouseEnter = function (num) {
             _this.setState({ index: num });
@@ -47,7 +47,7 @@ var StarList = function (_React$Component) {
                         return _this2.mouseLeave();
                     },
                     onClick: function onClick() {
-                        return postRatings(num);
+                        return _this2.props.onClick(num);
                     },
                     isHover: num <= _this2.state.index,
                     isFull: num <= _this2.state.rating && _this2.state.index == -1
@@ -58,15 +58,6 @@ var StarList = function (_React$Component) {
 
     return StarList;
 }(React.Component);
-
-// Steps moving forward: figure out how to send data to server and then retrieve it back to render
-// into the state of ratings
-
-
-function postRatings(rating) {
-    console.log(rating);
-    // console.log(LikeButton.state)
-}
 
 var Star = function Star(props) {
     return React.createElement(
@@ -99,11 +90,6 @@ var Star = function Star(props) {
 var LikeButton = function (_React$Component2) {
     _inherits(LikeButton, _React$Component2);
 
-    //   constructor(props) {
-    //     super(props);
-    //     this.state = { liked: false };
-    //   }
-
     function LikeButton() {
         _classCallCheck(this, LikeButton);
 
@@ -114,8 +100,13 @@ var LikeButton = function (_React$Component2) {
             return React.createElement(
                 "div",
                 { className: "ratings" },
-                "Class Enjoyment: ",
-                React.createElement(StarList, { rating: Math.round(ClassEnjoyment) }),
+                "Class Enjoyment:",
+                React.createElement(StarList, {
+                    rating: Math.round(ClassEnjoyment),
+                    onClick: function onClick(rating) {
+                        return _this3.userRating(rating);
+                    }
+                }),
                 ClassEnjoyment
             );
         };
@@ -179,57 +170,12 @@ var LikeButton = function (_React$Component2) {
                 });
             }, 500);
         }
-
-        // actual star component, is filled based on parameters, and each individual one is clicked 
-        // it sends a different rating to the database
-
     }, {
-        key: "star",
-        value: function star(full, index, type) {
-            var _this6 = this;
-
-            return React.createElement(
-                "svg",
-                {
-                    version: "1.1",
-                    xmlns: "http://www.w3.org/2000/svg",
-                    xmlnsXlink: "http://www.w3.org/1999/xlink",
-                    viewBox: "0 0 512 512",
-                    height: "2.0em",
-                    className: "star",
-                    onClick: function onClick() {
-                        if (type == "Class_Enjoyment") _this6.setState({ classEnjoyment: index }, _this6.postRatings());else console.log("different rating");
-                    }
-                },
-                React.createElement(
-                    "g",
-                    { transform: "scale(.95) translate(15,15)" },
-                    React.createElement("path", {
-                        stroke: "rgba(0,0,0,.85)",
-                        "stroke-width": "30",
-                        fill: full ? 'gold' : 'transparent',
-                        onMouseEnter: function onMouseEnter() {
-                            return _this6.fill = "transparent";
-                        },
-                        d: "M492.867,181.444l-149.825-21.785L276.014,23.861c-8.187-16.59-31.844-16.589-40.031,0l-67.026,135.799L19.133,181.445c-18.306,2.662-25.615,25.158-12.369,38.071l108.408,105.682L89.592,474.44c-3.125,18.232,16.012,32.136,32.386,23.528l132.475-70.452l134.025,70.451c17.914,8.607,37.051-5.296,33.926-23.528l-25.578-149.241l108.409-105.685C518.482,206.601,511.173,184.105,492.867,181.444z"
-                    })
-                )
-            );
+        key: "userRating",
+        value: function userRating(rating) {
+            this.setState({ classEnjoyment: rating });
+            this.postRatings();
         }
-
-        // Create the star ratings based on the score, and the type of rating
-
-    }, {
-        key: "createStarRatings",
-        value: function createStarRatings(rating, type) {
-            console.log("yp");
-            var stars = [];
-            for (var i = 1; i < 6; i++) {
-                if (i <= rating) stars.push(this.star(true, i, type));else stars.push(this.star(false, i, type));
-            }
-            return stars;
-        }
-
         // This renders the stars for class enjoyment, based on current state of the rating based on data from
         // database, it renders filled stars based on the rating, eg. rating = 4, then 4 gold stars
 
