bcdfaaa627cff3c7d90e80308f4821449918de34
Julian <julianmkchan@gmail.com>
2019-11-15 18:01:29 -0800

    will begin working on popup functionality

---
+++ PopupRatings.js
@@ -0,0 +1,3 @@
+// write a reactjs code that will count the seconds after component did mount 
+// and then have it pop up on the page
+// store in a cookie if the user has received a popup before so they only get one
