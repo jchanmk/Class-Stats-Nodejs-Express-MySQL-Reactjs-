47bd299c6edae1464532c95b45c7c1f0f94f3dcf
Julian <julianmkchan@gmail.com>
2019-11-20 22:41:23 -0800

    changed database config to online db

--- config/database.js
+++ config/database.js
@@ -1,8 +1,16 @@
+// module.exports = {
+//     'connection':{
+//         'host':'localhost',
+//         'user':'root',
+//         'password':'p56230101P'
+//     },
+//     'database':'ClassStats'
+// }
 module.exports = {
     'connection':{
-        'host':'localhost',
-        'user':'root',
-        'password':'p56230101P'
+        'host':'us-cdbr-iron-east-05.cleardb.net',
+        'user':'b430d34a9292e2',
+        'password':'a3eaa021'
     },
-    'database':'ClassStats'
+    'database':'heroku_ee6e96f9f6250a3'
 }
