33787875a24222ebd63df32924b35750967810ea
Julian <julianmkchan@gmail.com>
2019-11-21 22:58:05 -0800

    test

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 app.use(session({
     secret: 'justasecret',
