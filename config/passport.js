17f1346e2d4717a219b0299535c58db65f3080e8
Julian <julianmkchan@gmail.com>
2019-11-22 00:00:21 -0800

    test

--- config/passport.js
+++ config/passport.js
@@ -18,10 +18,14 @@ module.exports = function (passport) {
 
     passport.deserializeUser(function (email, done) {
         // console.log("in deserialize")
-        connection.query("SELECT * FROM Students WHERE email = ? ", [email],
-            function (err, rows) {
-                done(err, rows[0]);
-            });
+        pool.getConnection(function (err, connection) {
+            if (err) throw err;
+            connection.query("SELECT * FROM Students WHERE email = ? ", [email],
+                function (err, rows) {
+                    connection.release();
+                    done(err, rows[0]);
+                });
+        })
     });
 
     passport.use(
@@ -32,32 +36,40 @@ module.exports = function (passport) {
             passReqToCallback: true
         },
             function (req, email, password, done) {
-                connection.query("SELECT Password FROM Students WHERE Email = ? ",
-                    [email], function (err, rows) {
-                        if (err) {
-                            console.log("something went wrong");
-                            return done(err);
-                        }
-                        if (!rows.length) {   // if email was not found in database
-                            return done(null, false, req.flash('loginMessage', 'You are not in the database'));
-                        }
-                        if (rows.length && rows[0].Password != null) {
-                            return done(null, false, req.flash('loginMessage', 'You already have an account with us! Please login instead'));
-                        } else {
-                            const newUserMysql = {
-                                Email: email,
-                                password: bcrypt.hashSync(password, null, null)
-                            };
+                pool.getConnection(function (err, connection) {
+                    if (err) throw err;
+                    connection.query("SELECT Password FROM Students WHERE Email = ? ",
+                        [email], function (err, rows) {
+                            connection.release();
+                            if (err) {
+                                console.log("something went wrong");
+                                return done(err);
+                            }
+                            if (!rows.length) {   // if email was not found in database
+                                return done(null, false, req.flash('loginMessage', 'You are not in the database'));
+                            }
+                            if (rows.length && rows[0].Password != null) {
+                                return done(null, false, req.flash('loginMessage', 'You already have an account with us! Please login instead'));
+                            } else {
+                                const newUserMysql = {
+                                    Email: email,
+                                    password: bcrypt.hashSync(password, null, null)
+                                };
 
-                            const updateQuery = "UPDATE Students SET Password = ? WHERE Email = ?";
+                                const updateQuery = "UPDATE Students SET Password = ? WHERE Email = ?";
 
-                            connection.query(updateQuery, [newUserMysql.password, newUserMysql.Email], function (err, rows) {
-                                // console.log(rows);
-                                // newUserMysql.id = rows.insertId;
-                                return done(null, newUserMysql);
-                            });
-                        }
-                    })
+                                pool.getConnection(function (err, connection) {
+                                    if (err) throw err;
+                                    connection.query(updateQuery, [newUserMysql.password, newUserMysql.Email], function (err, rows) {
+                                        // console.log(rows);
+                                        // newUserMysql.id = rows.insertId;
+                                        connection.release();
+                                        return done(null, newUserMysql);
+                                    });
+                                })
+                            }
+                        })
+                })
             }
         )
     );
@@ -70,22 +82,26 @@ module.exports = function (passport) {
             passReqToCallback: true
         },
             function (req, email, password, done) {
-                connection.query("SELECT * FROM Students WHERE Email = ? ", [email],
-                    function (err, rows) {
-                        if (err) {
-                            return done(err);
-                        }
-                        if (!rows.length) {
-                            console.log("no user found");
-                            return done(null, false, req.flash('loginMessage', 'No User Found'))
-                        }
-                        if (!bcrypt.compareSync(password, rows[0].Password)) {
-                            console.log("wrong password");
-                            return done(null, false, req.flash('loginMessage', 'Wrong Password'))
-                        }
-                        console.log("right password");
-                        return done(null, rows[0]);
-                    });
+                pool.getConnection(function (err, connection) {
+                    if (err) throw err;
+                    connection.query("SELECT * FROM Students WHERE Email = ? ", [email],
+                        function (err, rows) {
+                            connection.release()
+                            if (err) {
+                                return done(err);
+                            }
+                            if (!rows.length) {
+                                console.log("no user found");
+                                return done(null, false, req.flash('loginMessage', 'No User Found'))
+                            }
+                            if (!bcrypt.compareSync(password, rows[0].Password)) {
+                                console.log("wrong password");
+                                return done(null, false, req.flash('loginMessage', 'Wrong Password'))
+                            }
+                            console.log("right password");
+                            return done(null, rows[0]);
+                        });
+                })
             })
     );
 };
