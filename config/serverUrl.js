c774cd18dd44874926215f428476ba8da5962f93
Julian <julianmkchan@gmail.com>
2019-12-03 18:43:15 -0800

    testing out online server

--- config/serverUrl.js
+++ config/serverUrl.js
@@ -1,4 +1,4 @@
-var serverURL = "http://localhost:3000";
-// var serverURL = "https://secret-dusk-64113.herokuapp.com";
+// var serverURL = "http://localhost:3000";
+var serverURL = "https://secret-dusk-64113.herokuapp.com";
 
 export default serverURL;
