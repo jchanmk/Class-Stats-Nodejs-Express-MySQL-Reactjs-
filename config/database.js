16268237efb5e6dfd145e2dd027346b032aab138
Julian <julianmkchan@gmail.com>
2019-12-11 17:12:56 -0800

    changing db config to neutral for other people to run

--- config/database.js
+++ config/database.js
@@ -1,29 +1,9 @@
-// module.exports = {
-//     'connection':{
-//         'host':'localhost',
-//         'user':'root',
-//         'password':'p56230101P'
-//     },
-//     'database':'ClassStats'
-// }
-
-// module.exports = {
-//     'connection':{
-//         connectionLimit: 70,
-//         host:'us-cdbr-iron-east-05.cleardb.net',
-//         user:'b430d34a9292e2',
-//         password:'a3eaa021',
-//         database:'heroku_ee6e96f9f6250a3'
-//     }
-//     // 'database':'heroku_ee6e96f9f6250a3'
-// }
-
 module.exports = {
     'connection':{
         connectionLimit:10,
         host:'localhost',
         user:'root',
-        password:'p56230101P',
+        password:'',    // fill in your password here
         database:'ClassStats'
     }
 }
